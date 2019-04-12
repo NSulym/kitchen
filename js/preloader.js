f99fb08ac56da2bc8a2efeba9a8a64e02d0e6326
NSulym <nazar.sulym@ukr.net>
2019-04-12 12:49:11 +0300

    first commit

--- js/preloader.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function($) {
-    $(window).load(function() {
-        setTimeout(function() {
-            $('.loader').fadeOut('slow', function() {});
-        }, 2000);
-    });
-});
-
